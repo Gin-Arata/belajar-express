73cfcd6f2970515c9e0e0e29ce1f3c8b8e5d2d52
Gaco Razan Kamil <2241720091@student.polinema.ac.id>
2024-08-08 15:11:11 +0700

    Express - Basic Routing and Add Nodemon Packages

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Pages');
+});
+
+app.get('/testing', (req, res) => {
+    res.send('test Pages');
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
