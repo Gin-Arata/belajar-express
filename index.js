be3da02d690735cfad2db594f6428ac763260efb
Gaco Razan Kamil <2241720091@student.polinema.ac.id>
2024-08-09 15:52:36 +0700

    Grouping Route

--- index.js
+++ index.js
@@ -10,38 +10,39 @@ app.get('/about', (req, res) => {
     res.redirect('/users/1');
 });
 
-app.get('/users/:userId', (req, res) => {
-    const dataUser = [
-        {
-            id: 1,
-            name: 'John Doe',
-        },
-        {
-            id: 2,
-            name: 'Jane Doe',
-        },
-    ];
+app.route('/users')
+    .get((req, res) => {
+        const dataUser = [
+            {
+                id: 1,
+                name: 'John Doe',
+            },
+            {
+                id: 2,
+                name: 'Jane Doe',
+            },
+        ];
 
-    const selectedData = dataUser.find(user => user.id === parseInt(req.params.userId));
+        const selectedData = dataUser.find(user => user.id === parseInt(req.params.userId));
 
-    if(!selectedData) {
-        res.status(404).send('404 - Data not found');
-    } else {
-        res.json(selectedData);
-    }
-});
+        if (!selectedData) {
+            res.status(404).send('404 - Data not found');
+        } else {
+            res.json(selectedData);
+        }
+    })
 
-app.post('/users/:userId', (req, res) => {
-    res.send('POST User ' + req.params.userId);
-});
+    .post((req, res) => {
+        res.send('POST User ' + req.params.userId);
+    })
 
-app.put('/users/:userId', (req, res) => {
-    res.send('PUT User ' + req.params.userId);
-});
+    .put((req, res) => {
+        res.send('PUT User ' + req.params.userId);
+    })
 
-app.delete('/users/:userId', (req, res) => {
-    res.send('Delete User ' + req.params.userId);
-});
+    .delete((req, res) => {
+        res.send('Delete User ' + req.params.userId);
+    });
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
