00f26d021b1ffbf7a6422f534dd6168d680b556e
Gaco Razan Kamil <2241720091@student.polinema.ac.id>
2024-08-09 14:55:57 +0700

    Route Parameter

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ app.get('/about', (req, res) => {
     res.send('About Pages');
 });
 
-app.get('/users', (req, res) => {
-    res.send('GET User');
+app.get('/users/:userId', (req, res) => {
+    res.send('GET User ' + req.params.userId);
 });
 
-app.post('/users', (req, res) => {
-    res.send('POST User');
+app.post('/users/:userId', (req, res) => {
+    res.send('POST User ' + req.params.userId);
 });
 
-app.put('/users', (req, res) => {
-    res.send('PUT User');
+app.put('/users/:userId', (req, res) => {
+    res.send('PUT User ' + req.params.userId);
 });
 
-app.delete('/users', (req, res) => {
-    res.send('Delete User');
+app.delete('/users/:userId', (req, res) => {
+    res.send('Delete User ' + req.params.userId);
 });
 
 app.listen(3000, () => {
